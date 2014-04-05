ab4e6717e7bdbd59e4db15a205ca3cde2609051e
jongrover <hello@jonathangrover.com>
2014-04-04 23:32:15 -0400

    remove solution

--- js/donut.js
+++ js/donut.js
@@ -1,34 +1,5 @@
 $(function(){
 
-  var Donut = {
-    type: 'glazed',
-    color: 'white',
-    tasty: true,
-    el: function() {
-      return '<li>'+
-                'type: '+this.type+'<br>'+
-                'color: '+this.color+'<br>'+
-                'tasty: '+this.tasty+
-             '</li>';
-    },
-    create: function() {
-      $('#donut-list').append(this.el());
-    }
-  };
-
-  Donut.all = [];
-
-  $('#create').click(function(){
-    var type = $('#type').val();
-    var color = $('#color').val();
-    var tasty = $('#tasty').val();
-    var newDonut = Object.create(Donut);
-    newDonut.type = type;
-    newDonut.color = color;
-    newDonut.tasty = tasty;
-    Donut.all.push(newDonut);
-    //console.log(Donut.all);
-    newDonut.create();
-  });
+  //write code here...
 
 });
