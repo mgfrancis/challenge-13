beabc7930493351eb1aae02a2ab0f5b6c1c8545c
mgfrancis <opusmla@gmail.com>
2014-04-05 14:42:28 -0400

    update js file

--- js/donut.js
+++ js/donut.js
@@ -1,5 +1,44 @@
 $(function(){
+ 	
+ 	function Donut(type,color,tasty){
 
-  //write code here...
+		this.type = type;
+		this.color = color;
+		this.tasty = tasty;
 
-});
+		this.el = function(){
+			return "<li>"+
+					this.type+"<br>"+
+					this.color+"<br>"+
+					this.tasty+"<br>"+
+					"</li>";
+		};
+
+		this.create = function(){
+			$('#donut-list').append(this.el());
+		};
+	}
+
+
+ 	//user clicks create - requires code
+
+ 	$('#create').click(function(){
+
+ 		//get the values they typed in -requires code
+
+ 		//user types input - does not require code
+
+ 		var type = $('#type').val();
+ 		var color = $('#color').val();
+ 		var tasty = $('#tasty').val();
+
+ 		//create a new donut based on Donut-requires code
+
+ 		var donut = new Donut(type, color, tasty);
+
+ 		//append the donut into the ul
+
+ 		donut.create();
+ 	});
+
+ });	
